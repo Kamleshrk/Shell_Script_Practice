9a01afdaf5621659c342b59bfef52b20ab33b9ac
Redhat <kkhairnar63@gmail.com>
2021-10-18 05:07:03 +0000

    1st

---
+++ folder-echo.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+mkdir /home/ktk/test-sh
+touch /home/ktk/test-sh/test.txt
+echo "run sucessfull" >> /home/ktk/test-sh/test.txt
---
+++ quotes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Purpose:Quotes Test
+#Version:1.0
+
+#Testing Double Quotes
+Krk=12345
+Trk=ABCD
+Hostname=$(hostname)
+echo "Print Double quotes $Krk"
+echo 'Print Single Quotes $Trk'
+echo "Test Reverse Quotes: $Hostname"
+
+
